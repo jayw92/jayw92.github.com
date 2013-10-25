c5b5373146482604130b6a028053588feebdabd2
Jay Wang <jayw.92@gmail.com>
2013-10-24 23:11:30 -0400

    fixed innerhtml

--- html/update.js
+++ html/update.js
@@ -5,17 +5,16 @@ function handleAPILoaded() {
 
 // Search for a given string.
 function search() {
-   var videoID = $('#vidId').val();
+   videoID = $('#vidId').val();
    var request = gapi.client.youtube.videos.list({
       id: videoID,
       part: 'snippet'
    });
 
    request.execute(function(response) {
-      var link = "//www.youtube.com/embed/" + videoID
+      link = "//www.youtube.com/embed/" + videoID
       var str = JSON.stringify(response.result);
       data = JSON.parse(request.responseText);
-
-      $('#search-container').html(<iframe width="480" height="360" src=link frameborder="0" allowfullscreen></iframe>);
+      document.getElementById("search-container").innerHTML = "<iframe width="480" height="360" src=link frameborder="0" allowfullscreen></iframe>";
    });
 }
