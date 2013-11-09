53e78d5561d68228b7b5eff03e0e1d240b10468d
Jay Wang <jayw.92@gmail.com>
2013-11-09 09:48:25 -0500

    removed playlist.js

--- html/js/update.js
+++ html/js/update.js
@@ -1,10 +1,78 @@
 // Once the api loads call enable the search box.
 function handleAPILoaded() {
 //    $('#search-button').attr('disabled', false);
+    enableForm();
     var index = 0;
     categoryDropdownHTML = "<p class=\"updatefield\">Select Category: <select id=\"c_CategoryId\">\n";
     getCategoryTitle(index);
 }
+
+// Some variables to remember state.
+var playlistId, channelId;
+
+// Enable a form to create a playlist.
+function enableForm() {
+    $('#playlist-button').attr('disabled', false);
+}
+
+// Create a private playlist.
+function createPlaylist() {
+    var request = gapi.client.youtube.playlists.insert({
+        part: 'snippet,status',
+        resource: {
+            snippet: {
+                title: 'Test Playlist',
+                description: 'A private playlist created with the YouTube API'
+            },
+            status: {
+                privacyStatus: 'private'
+            }
+        }
+    });
+    request.execute(function(response) {
+        var result = response.result;
+        if (result) {
+            playlistId = result.id;
+            $('#playlist-id').val(playlistId);
+            $('#playlist-title').html(result.snippet.title);
+            $('#playlist-description').html(result.snippet.description);
+        } else {
+            $('#status').html('Could not create playlist');
+        }
+    });
+}
+
+// Add a video id from a form to a playlist.
+function addVideoToPlaylist() {
+    addToPlaylist($('#video-id').val());
+}
+
+// Add a video to a playlist.
+function addToPlaylist(id, startPos, endPos) {
+    var details = {
+        videoId: id,
+        kind: 'youtube#video'
+    }
+    if (startPos != undefined) {
+        details['startAt'] = startPos;
+    }
+    if (endPos != undefined) {
+        details['endAt'] = endPos;
+    }
+    var request = gapi.client.youtube.playlistItems.insert({
+        part: 'snippet',
+        resource: {
+            snippet: {
+                playlistId: playlistId,
+                resourceId: details
+            }
+        }
+    });
+    request.execute(function(response) {
+        $('#status').html('<pre>' + JSON.stringify(response.result) + '</pre>');
+    });
+}
+
 
 // GLOBAL VARIABLES
 var videoID, link, data, snippet, snip, statistics, status, fileDetails, processingDetails, suggestions;
