b3dbdddd393c1c5047dfc83dccdc368c514c5ade
Jay Wang <jayw.92@gmail.com>
2013-11-09 08:08:36 -0500

    add video list

--- html/js/update.js
+++ html/js/update.js
@@ -1,5 +1,6 @@
 // Once the api loads call enable the search box.
 function handleAPILoaded() {
+    requestUserUploadsPlaylistId();
     $('#search-button').attr('disabled', false);
 }
 
@@ -14,9 +15,74 @@ var v_UploadStatus, v_ViewCount;
 var videoHTML, newHTML, str, str2, str3;
 var u_str, updateRequest;
 var c_CategoryId, c_Description, c_License, c_PrivacyStatus, c_TagsList, c_Title;
+var playlistId, nextPageToken, prevPageToken, playlistItems;
+
 
 var cateSnip, CategoryTitleReturn;
 // UPDATE VIDEO FUNCTION
+
+//Retrieve the uploads playlist id.
+function requestUserUploadsPlaylistId() {
+    // https://developers.google.com/youtube/v3/docs/channels/list
+    var request = gapi.client.youtube.channels.list({
+        mine: true,
+        part: 'contentDetails'
+    });
+    request.execute(function(response) {
+        playlistId = response.result.items[0].contentDetails.relatedPlaylists.uploads;
+        requestVideoPlaylist(playlistId);
+    });
+}
+
+// Retrieve a playist of videos.
+function requestVideoPlaylist(playlistId, pageToken) {
+    $('#video-container').html('');
+    var requestOptions = {
+        playlistId: playlistId,
+        part: 'snippet',
+        maxResults: 10
+    };
+    if (pageToken) {
+        requestOptions.pageToken = pageToken;
+    }
+    var request = gapi.client.youtube.playlistItems.list(requestOptions);
+    request.execute(function(response) {
+        // Only show the page buttons if there's a next or previous page.
+        nextPageToken = response.result.nextPageToken;
+        var nextVis = nextPageToken ? 'visible' : 'hidden';
+        $('#next-button').css('visibility', nextVis);
+        prevPageToken = response.result.prevPageToken
+        var prevVis = prevPageToken ? 'visible' : 'hidden';
+        $('#prev-button').css('visibility', prevVis);
+
+        playlistItems = response.result.items;
+        if (playlistItems) {
+            $.each(playlistItems, function(index, item) {
+                displayResult(item.snippet);
+            });
+        } else {
+            $('#video-container').html('Sorry you have no uploaded videos');
+        }
+    });
+}
+
+// Create a thumbnail for a video snippet.
+function displayResult(videoSnippet) {
+  var title = videoSnippet.title;
+  var videoId = videoSnippet.resourceId.videoId;
+  $('#video-container').append('<p>' + title + ' - ' + videoId + '</p>');
+}
+
+// Retrieve the next page of videos.
+function nextPage() {
+  requestVideoPlaylist(playlistId, nextPageToken);
+}
+
+// Retrieve the previous page of videos.
+function previousPage() {
+  requestVideoPlaylist(playlistId, prevPageToken);
+}
+
 
 function updateVideo() {
     c_Title = $('#c_Title').val();
